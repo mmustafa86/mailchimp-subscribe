e0ee711fa65e77d223f028963810fa4587e15450
mohammed mustafa <mohammed.alkhddar@gmail.com>
2021-03-10 20:49:56 -0600

    test app

---
+++ app.js
@@ -0,0 +1,82 @@
+//
+const express = require('express');
+const bodyParser =require('body-parser');
+const request= require('request');
+const https =require('https');
+const { url } = require('inspector');
+
+const app=express();
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/',function(req,res) {
+    
+    res.sendFile(__dirname +'/signup.html' )
+})
+
+
+app.post('/',function(req,res) {
+    const {fName} =req.body
+    const {lName} =req.body
+    const email =req.body.email
+    console.log(fName ,email,lName)
+
+
+    ///  API set 
+var data={
+    members:[
+      {
+        email_address :email ,
+        status:"subscribed",
+        merge_fields: {
+            FNAME :fName,
+            LNAME: lName
+        }
+      }  
+    ] ,
+
+}
+
+const jsonData = JSON.stringify(data)
+
+const url ="https://us1.api.mailchimp.com/3.0/lists/8553b00b89"
+
+const options= {
+    method:"POST",
+    auth:"mok2012:4ed40b07185071c0222fb0da24bb8bbf-us1"
+}
+
+const request =https.request(url ,options, function (response) {
+
+
+if (response.statusCode===200){
+    res.sendFile(__dirname +'/success.html' )
+    
+}else {
+    res.sendFile(__dirname +'/failure.html' )
+}
+
+
+    response.on("data",function (data) {
+        console.log(JSON.parse(data))
+    })
+    
+})
+// console.log(jsonData)
+    // request.write(jsonData)
+    request.end()
+})
+
+app.post('/failure',function(req,res) {
+res.redirect("/")
+
+})
+
+app.listen('8080',function(){
+    console.log('Server is running on port 8080')
+})
+//api key
+//4ed40b07185071c0222fb0da24bb8bbf-us1
+//list id 
+//8553b00b89
